055ae1241d6cb4f694188485d6b4d9cfda6e13f4
ridho789 <ridhoalfian808@gmail.com>
2024-02-12 15:44:46 +0700

    update add table bill recaps database migrate

---
+++ database/migrations/2024_02_12_083344_create_table_bill_recaps.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tbl_bill_recaps', function (Blueprint $table) {
+            $table->id('id_bill_recap');
+            $table->date('load_date');
+            $table->string('no_inv');
+            $table->string('freight_type');
+            $table->string('size');
+            $table->string('unit_price');
+            $table->string('amount');
+            $table->date('payment_date');
+            $table->string('payment_amount');
+            $table->string('remaining_bill')->nullable();
+            $table->date('overdue_bill')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tbl_bill_recaps');
+    }
+};
