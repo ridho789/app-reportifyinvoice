580dd655ffddfa6f43807dc64c75e617683b9f2e
ridho789 <ridhoalfian808@gmail.com>
2024-03-14 14:48:38 +0700

    update form view sea shipment

--- resources/views/shipment/sea_shipment/form_sea_shipment.blade.php
+++ resources/views/shipment/sea_shipment/form_sea_shipment.blade.php
@@ -13,13 +13,13 @@
                 </div>
                 <div class="card-body px-4 pt-0 pb-0 mb-3">
                     <div class="table-responsive">
-                        <table class="table table-bordered align-items-center mb-0">
+                        <table class="table align-items-center mb-0">
                             <thead>
                                 <tr>
                                     <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7" rowspan="2">No.</th>
                                     <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7" rowspan="2">BL Date</th>
-                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7" rowspan="2">Total Packages <br> pkgs</th>
-                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7" rowspan="2">Total Weight <br> kgs</th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7" rowspan="2">Total Packages</th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7" rowspan="2">Total Weight</th>
                                     <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7" rowspan="1" colspan="3">Total CBM</th>
                                     <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7" rowspan="2">Description</th>
                                 </tr>
@@ -85,62 +85,65 @@
                 </div>
                 <div class="card-body px-4 pt-0 pb-0">
                     <div class="table-responsive">
-                        <table class="table table-bordered align-items-center mb-0">
-                            <thead>
-                                <tr>
-                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">No.</th>
-                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Date</th>
-                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Shipper</th>
-                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Customer</th>
-                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Ship</th>
-                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Total Ships</th>
-                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Total Packages</th>
-                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Total Weight</th>
-                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Total Volume</th>
-                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Etd</th>
-                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Eta</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>
-                                        <div class="d-flex px-3 py-1">
-                                            <p class="text-xs text-secondary mb-0">-</p>
-                                        </div>
-                                    </td>
-                                    <td class="align-middle text-center">
-                                        <p class="text-xs text-secondary mb-0">-</p>
-                                    </td>
-                                    <td class="align-middle text-center">
-                                        <span class="text-secondary text-xs font-weight-normal">-</span>
-                                    </td>
-                                    <td class="align-middle text-center">
-                                        <span class="text-secondary text-xs font-weight-normal">-</span>
-                                    </td>
-                                    <td class="align-middle text-center">
-                                        <span class="text-secondary text-xs font-weight-normal">-</span>
-                                    </td>
-                                    <td class="align-middle text-center">
-                                        <span class="text-secondary text-xs font-weight-normal">-</span>
-                                    </td>
-                                    <td class="align-middle text-center">
-                                        <span class="text-secondary text-xs font-weight-normal">-</span>
-                                    </td>
-                                    <td class="align-middle text-center">
-                                        <span class="text-secondary text-xs font-weight-normal">-</span>
-                                    </td>
-                                    <td class="align-middle text-center">
-                                        <span class="text-secondary text-xs font-weight-normal">-</span>
-                                    </td>
-                                    <td class="align-middle text-center">
-                                        <span class="text-secondary text-xs font-weight-normal">-</span>
-                                    </td>
-                                    <td class="align-middle text-center">
-                                        <span class="text-secondary text-xs font-weight-normal">-</span>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        <form id="form-sea-freight" method="POST" action="#">
+                            @csrf
+                            <table class="table table-bordered align-items-center mb-0">
+                                <thead>
+                                    <tr>
+                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">No.</th>
+                                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Date</th>
+                                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Shipper</th>
+                                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Customer</th>
+                                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Ship</th>
+                                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Total Ships</th>
+                                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Total Packages</th>
+                                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Total Weight</th>
+                                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Total Volume</th>
+                                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Etd</th>
+                                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Eta</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td width=7.5%>
+                                            <div class="d-flex px-3 py-1">
+                                                <input type="text" class="form-control" name="no[]" placeholder="...">
+                                            </div>
+                                        </td>
+                                        <td class="align-middle text-center" width=7.5%>
+                                            <input type="date" class="form-control" name="date[]">
+                                        </td>
+                                        <td class="align-middle text-center">
+                                            <span class="text-secondary text-xs font-weight-normal">-</span>
+                                        </td>
+                                        <td class="align-middle text-center">
+                                            <span class="text-secondary text-xs font-weight-normal">-</span>
+                                        </td>
+                                        <td class="align-middle text-center">
+                                            <span class="text-secondary text-xs font-weight-normal">-</span>
+                                        </td>
+                                        <td class="align-middle text-center">
+                                            <span class="text-secondary text-xs font-weight-normal">-</span>
+                                        </td>
+                                        <td class="align-middle text-center">
+                                            <span class="text-secondary text-xs font-weight-normal">-</span>
+                                        </td>
+                                        <td class="align-middle text-center">
+                                            <span class="text-secondary text-xs font-weight-normal">-</span>
+                                        </td>
+                                        <td class="align-middle text-center">
+                                            <span class="text-secondary text-xs font-weight-normal">-</span>
+                                        </td>
+                                        <td class="align-middle text-center">
+                                            <span class="text-secondary text-xs font-weight-normal">-</span>
+                                        </td>
+                                        <td class="align-middle text-center">
+                                            <span class="text-secondary text-xs font-weight-normal">-</span>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </form>
                     </div>
 
                     <div class="table-responsive py-3 mt-3">
@@ -175,55 +178,58 @@
                             </thead>
                             <tbody>
                                 <tr>
-                                    <td contenteditable="true">
+                                    <td width=2.5%>
                                         <div class="d-flex px-3 py-1">
-                                            <p class="text-xs text-secondary mb-0 font-weight-normal">-</p>
+                                            <input type="text" class="form-control text-center" placeholder="..." style="border: 0px;">
                                         </div>
                                     </td>
-                                    <td class="align-middle text-center" contenteditable="true">
-                                        <p class="text-xs text-secondary mb-0 font-weight-normal">-</p>
+                                    <td class="align-middle text-center" width=7.5%>
+                                        <input type="text" class="form-control text-center" name="date[]" placeholder="..." style="border: 0px;">
                                     </td>
-                                    <td class="align-middle text-center" contenteditable="true">
-                                        <p class="text-xs text-secondary mb-0 font-weight-normal">-</p>
+                                    <td class="align-middle text-center" width=7.5%>
+                                        <input type="text" class="form-control text-center" name="code[]" placeholder="..." style="border: 0px;">
                                     </td>
-                                    <td class="align-middle text-center" contenteditable="true">
-                                        <span class="text-secondary text-xs font-weight-normal">-</span>
+                                    <td class="align-middle text-center" width=10%>
+                                        <input type="text" class="form-control text-center" name="marking[]" placeholder="..." style="border: 0px;">
                                     </td>
                                     <!-- qty -->
-                                    <td class="align-middle text-center" contenteditable="true">
-                                        <span class="text-secondary text-xs font-weight-normal">-</span>
+                                    <td class="align-middle text-center" width=7.5%>
+                                        <input type="text" class="form-control text-center" name="qty_pkg[]" placeholder="..." style="border: 0px;">
                                     </td>
-                                    <td class="align-middle text-center" contenteditable="true">
-                                        <span class="text-secondary text-xs font-weight-normal">-</span>
+                                    <td class="align-middle text-center" width=7.5%>
+                                        <input type="text" class="form-control text-center" name="qty_loose[]" placeholder="..." style="border: 0px;">
                                     </td>
                                     <!-- ### -->
-                                    <td class="align-middle text-center" contenteditable="true">
-                                        <span class="text-secondary text-xs font-weight-normal">-</span>
+                                    <td class="align-middle text-center" width=10%>
+                                        <input type="text" class="form-control text-center" name="weight[]" placeholder="..." style="border: 0px;">
                                     </td>
                                     <!-- dimension -->
-                                    <td class="align-middle text-center" contenteditable="true">
-                                        <span class="text-secondary text-xs font-weight-normal">-</span>
+                                    <td class="align-middle text-center" width=5%>
+                                        <input type="text" class="form-control text-center" name="p[]" placeholder="..." style="border: 0px;">
                                     </td>
-                                    <td class="align-middle text-center" contenteditable="true">
-                                        <span class="text-secondary text-xs font-weight-normal">-</span>
+                                    <td class="align-middle text-center" width=5%>
+                                        <input type="text" class="form-control text-center" name="l[]" placeholder="..." style="border: 0px;">
                                     </td>
-                                    <td class="align-middle text-center" contenteditable="true">
-                                        <span class="text-secondary text-xs font-weight-normal">-</span>
+                                    <td class="align-middle text-center" width=5%>
+                                        <input type="text" class="form-control text-center" name="t[]" placeholder="..." style="border: 0px;">
                                     </td>
                                     <!-- ### -->
                                     <!-- total cbm -->
-                                    <td class="align-middle text-center" contenteditable="true">
-                                        <span class="text-secondary text-xs font-weight-normal">-</span>
+                                    <td class="align-middle text-center" width=5%>
+                                        <input type="text" class="form-control text-center" name="cbm1[]" placeholder="..." style="border: 0px;">
                                     </td>
-                                    <td class="align-middle text-center" contenteditable="true">
-                                        <span class="text-secondary text-xs font-weight-normal">-</span>
+                                    <td class="align-middle text-center" width=5%>
+                                        <input type="text" class="form-control text-center" name="cbm2[]" placeholder="..." style="border: 0px;">
                                     </td>
                                     <!-- ### -->
                                     <td class="align-middle text-center" contenteditable="true">
-                                        <span class="text-secondary text-xs font-weight-normal">-</span>
+                                        <input type="text" class="form-control" name="desc[]" placeholder="desc.." style="border: 0px;">
                                     </td>
                                     <td class="align-middle text-center" contenteditable="true">
-                                        <span class="text-secondary text-xs font-weight-normal">-</span>
+                                        <select class="form-select text-xs" name="state[]" style="border: 0px;">
+                                            <option value="hold">HOLD</option>
+                                            <option value="continue">CONTINUE</option>
+                                        </select>
                                     </td>
                                 </tr>
                             </tbody>
